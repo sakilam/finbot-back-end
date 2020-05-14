3a55c061883e325b2e2362dc496c5ee80910007d
Sandeep Sakilam <sandeep.sakilam@atmecs.com>
2020-05-14 22:03:59 +0530

    Removed authorization for fetch employee details route

--- resources/employee_detail.py
+++ resources/employee_detail.py
@@ -8,11 +8,11 @@ from resources.errors import InternalServerError, EmployeeDetailsNotExistsError
 
 class EmployeeDetailApi(Resource):
 
-    @jwt_required
+    #@jwt_required
     def get(self, id):
         try:
-            employee_id = get_jwt_identity()
-            employee = EmployeeDetails.objects.get(employee_id=employee_id).to_json()
+            #employee_id = get_jwt_identity()
+            employee = EmployeeDetails.objects.get(emp_id=int(id)).to_json()
             return Response(employee, mimetype="application/json", status=200)
         except DoesNotExist:
             raise EmployeeDetailsNotExistsError
